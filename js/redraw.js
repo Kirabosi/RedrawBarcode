627956ddb62ef7cc87351413a7c82d4d733ff9b7
Kirabosi <mailily1113@gmail.com>
2014-06-01 20:07:03 +0900

    バーコードの作成及び表示部分の追加
    バーコードの作成に必要そうな設定をインプットするHTMLの準備
    その設定からバーコードを生成表示する処理の追加

---
+++ js/redraw.js
@@ -0,0 +1,25 @@
+function createBarcode() {
+	var code,
+		type,
+		crc,
+		width,
+		height;
+	
+	code = $('#code').val();
+	type = $('#type option:selected').val();
+	width = $('#width').val() || 1;
+	height = $('#height').val() || 50;
+	
+	if ('std25' === type || 'int25' === type || 'code93' === type || 'msi' === type) {
+		if ('checked' === $('#crc:checked').val()) {
+			crc = true;
+		} else {
+			crc = false;
+		}
+		$("#output").barcode({code: code, crc: crc}, type, {barWidth: width, barHeight: height});
+	} else {
+		$("#output").barcode(code, type, {barWidth: width, barHeight: height});
+	}
+
+//	$("#output").barcode(code, type, {barWidth:width, barHeight:height});
+}
